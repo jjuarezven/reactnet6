9ad06016a496615448f252198f05c740da07d0bb
josejuarez <79205538+josejmill5@users.noreply.github.com>
2022-08-23 21:03:50 -0500

    Adding HouseDetail component

--- Api/ReactFrontEnd/src/hooks/houseHooks.ts
+++ Api/ReactFrontEnd/src/hooks/houseHooks.ts
@@ -1,11 +1,16 @@
 import axios, { AxiosError } from "axios";
-import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import { Config } from "../config";
 import { House } from "../models/House";
 
 export const useFetchHouses = () => {
-  return useQuery<House[], AxiosError>("houses", () =>
-    axios.get(`${Config.baseApiUrl}/houses`).then((resp) => resp.data)
-  );
+	return useQuery<House[], AxiosError>("houses", () =>
+		axios.get(`${Config.baseApiUrl}/houses`).then((resp) => resp.data)
+	);
+};
+
+export const useFetchHouse = (id: number) => {
+	return useQuery<House, AxiosError>(["houses", id], () =>
+		axios.get(`${Config.baseApiUrl}/houses/${id}`).then((resp) => resp.data)
+	);
 };
--- Api/ReactFrontEnd/src/main/App.tsx
+++ Api/ReactFrontEnd/src/main/App.tsx
@@ -1,21 +1,22 @@
-import React from "react";
 import "./App.css";
 import { Header } from "./Header";
 import { Constants } from "../common/constants";
 import { HouseList } from "../house/HouseList";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HouseDetail } from "../house/HouseDetail";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="container">
-        <Header subtitle={Constants.headerTitle}></Header>
-        <Routes>
-          <Route path="/" element={<HouseList />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="container">
+				<Header subtitle={Constants.headerTitle}></Header>
+				<Routes>
+					<Route path="/" element={<HouseList />}></Route>
+					<Route path="/house/:id" element={<HouseDetail />}></Route>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
