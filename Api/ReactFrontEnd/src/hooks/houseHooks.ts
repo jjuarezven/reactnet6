b8e4939dbd3794c16bc103cf6a6d59b3d8db3d53
josejuarez <79205538+josejmill5@users.noreply.github.com>
2022-08-28 16:55:51 -0500

    Adding new hooks for add, update and delete houses

--- Api/ReactFrontEnd/src/hooks/houseHooks.ts
+++ Api/ReactFrontEnd/src/hooks/houseHooks.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosError } from "axios";
-import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { Config } from "../config";
 import { House } from "../models/House";
 
@@ -12,5 +13,35 @@ export const useFetchHouses = () => {
 export const useFetchHouse = (id: number) => {
 	return useQuery<House, AxiosError>(["houses", id], () =>
 		axios.get(`${Config.baseApiUrl}/houses/${id}`).then((resp) => resp.data)
+	);
+};
+
+export const useAddHouse = () => {
+	const nav = useNavigate();
+	const queryClient = useQueryClient();
+
+	return useMutation<AxiosResponse, AxiosError, House>(
+		(h) => axios.post(`${Config.baseApiUrl}/houses`, h),
+		{
+			onSuccess: () => {
+				queryClient.invalidateQueries("houses");
+				nav("/");
+			},
+		}
+	);
+};
+
+export const useDeleteHouse = () => {
+	const nav = useNavigate();
+	const queryClient = useQueryClient();
+
+	return useMutation<AxiosResponse, AxiosError, House>(
+		(h) => axios.delete(`${Config.baseApiUrl}/houses/${h.id}`),
+		{
+			onSuccess: (_, house) => {
+				queryClient.invalidateQueries("houses");
+				nav(`/house/${house.id}`);
+			},
+		}
 	);
 };
